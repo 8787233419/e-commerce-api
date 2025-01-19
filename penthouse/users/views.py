4dcd3ae4662187dd21437cc1d7039bf7bf696abe
G-Sohal <gnrsohal@gmail.com>
2025-01-19 11:32:48 +0000

    Added templates folder

--- penthouse/users/views.py
+++ penthouse/users/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def front(request) :
+#     return render(request, '#the html file')
+
+# Instead of a html file (dne till now) so instead a http response
 def taskList(request) : 
     return HttpResponse('Ok?')
