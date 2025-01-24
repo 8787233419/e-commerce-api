9b298cb8d84519931ffe02945836e4564501adb5
G-Sohal <gnrsohal@gmail.com>
2025-01-24 14:57:51 +0000

    Changed model field references

--- penthouse/users/admin.py
+++ penthouse/users/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import Member
-from .models import userlogin
-from .models import pwd_history
+from .models import UserLogin
+from .models import PwdHistory
 from .models import Category
-from .models import product
-from .models import order
+from .models import Product
+from .models import Order
 
 # Register your models here.
 admin.site.register(Member)
-admin.site.register(userlogin)
-admin.site.register(pwd_history)
+admin.site.register(UserLogin)
+admin.site.register(PwdHistory)
 admin.site.register(Category)
-admin.site.register(product)
-admin.site.register(order)
+admin.site.register(Product)
+admin.site.register(Order)
--- penthouse/users/models.py
+++ penthouse/users/models.py
@@ -16,64 +16,49 @@ class Member(models.Model) :
     def __str__(self) :
         return self.user_id
 
-class userlogin(models.Model) :
-    user_id = models.CharField(max_length = 50)
-    full_name = models.CharField(max_length = 200)
+class UserLogin(models.Model) :
+    user = models.OneToOneField(Member, on_delete = models.CASCADE, null = True)
     password = models.CharField(max_length = 16, validators = [MinLengthValidator(8)])
 
     def __str__(self) :
-        return self.full_name
+        return self.user.user_id
 
 
-class pwd_history(models.Model):
-    user_id=models.IntegerField()
+class PwdHistory(models.Model):
+    user = models.ForeignKey(Member, on_delete = models.CASCADE, null = True)
     creation_date = models.DateTimeField()
     password= models.CharField(max_length = 16, validators = [MinLengthValidator(8)])
 
     def __str__(self):
-        return self.user_id
+        return self.user.user_id
     
 #models for products, their categories, pricing, history etc
     
 class Category(models.Model):
-    cat_id=models.IntegerField(unique=True)
+    cat_id = models.IntegerField(unique=True)
     cat_name = models.CharField(max_length=255, unique=True)
     desc= models.CharField(max_length=500)
 
     def __str__(self):
         return self.cat_name
     
-class product(models.Model):
-    cat_id=models.IntegerField(unique=True)
-    cat_name = models.CharField(max_length=255, unique=True)
-    prod_id=models.IntegerField(unique=True)
-    prod_name=models.CharField(max_length=255)
-    prod_desc=models.CharField(max_length=500)
-    price=models.IntegerField()
-    stock_qty=models.IntegerField()
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, null = True)
+    prod_id = models.IntegerField(unique=True)
+    prod_name = models.CharField(max_length=255)
+    prod_desc = models.CharField(max_length=500)
+    price = models.IntegerField()
+    stock_qty = models.IntegerField()
 
     def __str__(self):
         return self.prod_name
     
 #models for order,payment,orderlist,shipping etc
-class order(models.Model):
-    prod_id=models.IntegerField(unique=True)
-    prod_name=models.CharField(max_length=255)
-    price=models.IntegerField()
-    qty=models.IntegerField()
-    order_id=models.IntegerField()
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, null = True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    qty = models.IntegerField()
+    order_id = models.IntegerField()
 
     def __str__(self):
         return self.order_id
-
-
-    
-
-    
-
-    
-
-    
-
-
-
