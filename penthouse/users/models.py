276f9a5c07656094754f3df9ddf043f70ce3275b
G-Sohal <gnrsohal@gmail.com>
2025-01-24 16:53:13 +0000

    Updated model fields

--- penthouse/users/models.py
+++ penthouse/users/models.py
@@ -1,6 +1,6 @@
 from itertools import product
 from django.db import models
-from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.core.validators import MaxValueValidator , MinValueValidator,MinLengthValidator
 
 # Create your models here.
 # pw boolfor admin --- remaining
@@ -10,7 +10,7 @@ class Member(models.Model) :
     user_id = models.CharField(max_length = 50)
     full_name = models.CharField(max_length = 200)
     address = models.CharField(max_length = 500)
-    mobile_no = models.IntegerField()
+    mobile_no = models.IntegerField( validators = [MinValueValidator(6666666666),MaxValueValidator(9999999999)])
     password = models.CharField(max_length = 16, validators = [MinLengthValidator(8)])
 
     def __str__(self) :
@@ -38,15 +38,19 @@ class Category(models.Model):
     cat_id = models.IntegerField(unique=True)
     cat_name = models.CharField(max_length=255, unique=True)
     desc= models.CharField(max_length=500)
+    cat_img = models.ImageField( upload_to = './images', default = './images/template.png')
 
     def __str__(self):
         return self.cat_name
+    def category_img(self):
+        return self.cat_img
     
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete = models.CASCADE, null = True)
     prod_id = models.IntegerField(unique=True)
     prod_name = models.CharField(max_length=255)
     prod_desc = models.CharField(max_length=500)
+    product_img = models.ImageField(upload_to = './images', default = './images/template.png')
     price = models.IntegerField()
     stock_qty = models.IntegerField()
 
