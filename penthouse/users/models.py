1a33374b3455297a83847bcc2527b2fb55fa378f
Xianbbx <xiaan0007@gmail.com>
2025-01-27 22:08:27 +0530

    added a foreign key to orders

--- penthouse/users/models.py
+++ penthouse/users/models.py
@@ -63,6 +63,7 @@ class Order(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     qty = models.IntegerField()
     order_id = models.IntegerField()
+    user=models.ForeignKey(Member,on_delete=models.CASCADE,null=True)
 
     def __str__(self):
          return self.order_id
