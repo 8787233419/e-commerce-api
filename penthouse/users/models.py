444423bb9284851799ae5b33041028f9c10b0e8b
Xianbbx <xiaan0007@gmail.com>
2025-01-29 14:44:31 +0530

    "orders model"

--- penthouse/users/models.py
+++ penthouse/users/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MaxValueValidator , MinValueValidator,MinLeng
 
 #create models regarding userid, userlogin, password history and other data regarding users
 class Member(models.Model) :
-    user_id = models.CharField(max_length = 50)
+    user_id = models.CharField(primary_key=True,max_length = 50)
     full_name = models.CharField(max_length = 200)
     address = models.CharField(max_length = 500)
     mobile_no = models.IntegerField( validators = [MinValueValidator(6666666666),MaxValueValidator(9999999999)])
@@ -59,11 +59,11 @@ class Product(models.Model):
     
 #models for order,payment,orderlist,shipping etc
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete = models.CASCADE, null = True)
+    product_id = models.ForeignKey(Product, on_delete = models.CASCADE, null = True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     qty = models.IntegerField()
     order_id = models.CharField(primary_key=True,max_length=25)
-    user=models.ForeignKey(Member,on_delete=models.CASCADE,null=True)
+    user_id=models.ForeignKey(Member,on_delete=models.CASCADE,null=True)
 
     def __str__(self):
          return self.order_id
